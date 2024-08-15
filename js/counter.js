d675d63edc9011a19be774626f59449b9be454ee
Shubham <129274040+Anheuserly@users.noreply.github.com>
2024-08-15 23:28:14 +0530

    Add files via upload

---
+++ js/counter.js
@@ -0,0 +1,24 @@
+// Animated counter
+const counters = document.querySelectorAll('.stat-number');
+    const speed = 200; // The lower the number, the faster the animation
+
+    const formatNumber = (num) => {
+        return num.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+    };
+
+    counters.forEach(counter => {
+        const updateCount = () => {
+            const target = +counter.getAttribute('data-target');
+            const count = +counter.innerText.replace(/,/g, '');
+            const inc = target / speed;
+
+            if (count < target) {
+                counter.innerText = formatNumber(Math.ceil(count + inc));
+                setTimeout(updateCount, 1);
+            } else {
+                counter.innerText = formatNumber(target);
+            }
+        };
+
+        updateCount();
+    });
